3aac5e584e5e5fb0fc3fd94bea9b7a06810d3ec2
EvergreenSpock <114445048+EvergreenSpock@users.noreply.github.com>
2023-03-19 19:55:52 -0700

    old car_production and primitives files

--- src/car_production.rs
+++ src/car_production.rs
@@ -1,14 +1,14 @@
-fn production_rate_per_hour(cars: f64) {
+fn production_rate_per_hour(cars: f32) {
     if cars <= 4.0 {
         println!("{}", cars * 221.0);
     }
     if cars <= 8.0 {
-        println!("{}", cars * (221.0 * .9));
+        println!("{}", cars * (221.0 * 0.9));
     }
     if cars <= 10.0 {
-        println!("{}", cars * (221.0 * .77));
+        println!("{}", cars * (221.0 * 0.77));
     }
 }
 fn main() {
-    production_rate_per_hour(3.0);
+    production_rate_per_hour(11.0);
 }
--- src/primitives.rs
+++ src/primitives.rs
@@ -1,6 +1,7 @@
 fn main() {
     let mut mutable = 12;
     mutable = 21;
+    println!("{}", mutable);
     let mutable = true;
     println!("{}", mutable);
     let mutable = false;
